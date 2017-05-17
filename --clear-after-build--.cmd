9472905e404b6179409342fce033d1294b774a5c
zamgi <denisov72@mail.ru>
2017-05-17 14:37:12 +0300

    Update --clear-after-build--.cmd

--- --clear-after-build--.cmd
+++ --clear-after-build--.cmd
@@ -3,6 +3,8 @@ del "*.csproj.user" /S/Q/F/A
 del "pos-tagger_ru.web.demo\bin\*.pdb" /Q
 rd "pos-tagger_ru.web.demo\obj" /S/Q
 rd "pos-tagger_ru.web.demo\(logs)" /S/Q
+del "pos-tagger_ru.console.demo\bin\*.pdb" /Q
+rd "pos-tagger_ru.console.demo\obj" /S/Q
 rd "captcha\bin" /S/Q
 rd "captcha\obj" /S/Q
 rd "crfsuite.net\bin" /S/Q
